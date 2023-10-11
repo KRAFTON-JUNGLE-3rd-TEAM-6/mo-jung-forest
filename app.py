e0fbc9a6b3865a29434132e4cc82df5338680e39
김상우 <electronyoon@gmail.com>
2023-10-11 20:04:43 +0900

    env 파일 사용, ec2의 mongodb 연결

--- app.py
+++ app.py
@@ -1,20 +1,30 @@
-from flask import Flask
+# import for flask app
+from flask import Flask, render_template, jsonify
 from pymongo import MongoClient
-import json
+from bson.objectid import ObjectId
 from flask.json.provider import JSONProvider
+import json
 
+# import for environment variables
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
+# start app and connect to db
 app = Flask(__name__)
+client = MongoClient('3.34.130.34',     # 배포 시 localhost로 변경
+                    27017,
+                    username = os.getenv('MONGO_ID'),
+                    password = os.getenv('MONGO_PW'),
+                    authSource = os.getenv('MONGO_AUTH'))
+db = client.mojungforest
 
-client = MongoClient('localhost', 27017)
-db = client.dbjungle    # DB 이름 변경
-
-###
+# custom (de)serializer for ObjectId
 class CustomJSONEncoder(json.JSONEncoder):
     def default(self, o):
         if isinstance(o, ObjectId):
             return str(o)
         return json.JSONEncoder.default(self, o)
-
 
 class CustomJSONProvider(JSONProvider):
     def dumps(self, obj, **kwargs):
@@ -23,14 +33,13 @@ class CustomJSONProvider(JSONProvider):
     def loads(self, s, **kwargs):
         return json.loads(s, **kwargs)
 
-
 app.json = CustomJSONProvider(app)
-###
+
+
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
-
 
 if __name__ == '__main__':
     app.run('127.0.0.1', port=5000, debug=True)

75194b89dfa3804b7c4b8b2f370f4facf15676e2
김상우 <electronyoon@gmail.com>
2023-10-13 11:16:25 +0900

    호스팅 정보 일부 수정(로컬과 배포환경에서 동일하게 작동하도록)

--- app.py
+++ app.py
@@ -505,4 +505,4 @@ def validate_token(access_token, refresh_token):
 
 
 if __name__ == '__main__':
-    app.run('127.0.0.1', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
